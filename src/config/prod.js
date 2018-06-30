2dc7d8fd3b1e15da0fa82b3d030f4f59c5a6f13e
Mikees23 <michael23.jd@gmail.com>
2018-06-30 21:19:26 +0600

    Added config of prod

---
+++ src/config/prod.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyBpCp0HM6vLNqSwZ_oWBEu-bXF4en6xeS8",
+  authDomain: "prod-todo-react.firebaseapp.com",
+  databaseURL: "https://prod-todo-react.firebaseio.com",
+};
