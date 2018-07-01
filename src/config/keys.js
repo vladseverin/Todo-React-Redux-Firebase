050b3f047fb956992b2940cd2f32f0e6c970b81a
Mikees23 <michael23.jd@gmail.com>
2018-07-01 14:21:32 +0600

    Change config variable

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 export const FirebaseConfig = {
   apiKey: process.env.API_KEY || "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
   authDomain: process.env.AUTH_DOMAIN || "react-redux-firebase-6ae9c.firebaseapp.com",
-  databaseURL: process.env.DATA_BASE_URL || "https://react-redux-firebase-6ae9c.firebaseio.com",
+  databaseURL: process.env.DATABASE_URL || "https://react-redux-firebase-6ae9c.firebaseio.com",
 }
