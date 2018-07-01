b4d556c33a2d0129c60b798a8b2e8d2334064388
Mikees23 <michael23.jd@gmail.com>
2018-07-01 17:13:25 +0600

    Try again change config var

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 export const FirebaseConfig = {
-  apiKey: process.env.API_KEY || "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
-  authDomain: process.env.AUTH_DOMAIN || "react-redux-firebase-6ae9c.firebaseapp.com",
-  databaseURL: process.env.DATABASE_URL || "https://react-redux-firebase-6ae9c.firebaseio.com",
+  apiKey: process.env.NODE_ENV === 'production' ? process.env.API_KEY : "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
+  authDomain: process.env.NODE_ENV === 'production' ? process.env.AUTH_DOMAIN : "react-redux-firebase-6ae9c.firebaseapp.com",
+  databaseURL: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : "https://react-redux-firebase-6ae9c.firebaseio.com",
 }
