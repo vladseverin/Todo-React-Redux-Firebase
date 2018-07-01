9cc7d8ba3a0bb764cb742cf51c176aaca2c85679
Mikees23 <michael23.jd@gmail.com>
2018-07-01 14:09:30 +0600

    Create config vars

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
-} else {
-  module.exports = require("./dev");
+export const FirebaseConfig = {
+  apiKey: process.env.API_KEY || "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
+  authDomain: process.env.AUTH_DOMAIN || "react-redux-firebase-6ae9c.firebaseapp.com",
+  databaseURL: process.env.DATA_BASE_URL || "https://react-redux-firebase-6ae9c.firebaseio.com",
 }
--- src/config/prod.js
+++ src/config/prod.js
@@ -2,7 +2,4 @@ export const FirebaseConfig = {
   apiKey: "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
   authDomain: "react-redux-firebase-6ae9c.firebaseapp.com",
   databaseURL: "https://react-redux-firebase-6ae9c.firebaseio.com",
-  projectId: "react-redux-firebase-6ae9c",
-  storageBucket: "react-redux-firebase-6ae9c.appspot.com",
-  messagingSenderId: "275018728347"
 };
