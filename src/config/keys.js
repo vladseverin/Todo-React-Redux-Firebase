642e0dd69c0928565fbdc80787b489ab9cf7b573
Mikees23 <michael23.jd@gmail.com>
2018-07-01 17:42:58 +0600

    Fix bug on heroku

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,9 +1,9 @@
-export const API_KEY = process.env.API_KEY;
-export const AUTH_DOMAIN = process.env.AUTH_DOMAIN;
-export const DATABASE_URL = process.env.DATABASE_URL;
+export const API_KEY = process.env.API_KEY || "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ";
+export const AUTH_DOMAIN = process.env.AUTH_DOMAIN || "react-redux-firebase-6ae9c.firebaseapp.com";
+export const DATABASE_URL = process.env.DATABASE_URL || "https://react-redux-firebase-6ae9c.firebaseio.com";
 
 export const FirebaseConfig = {
-  apiKey: process.env.NODE_ENV === 'production' ? API_KEY : "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
-  authDomain: process.env.NODE_ENV === 'production' ? AUTH_DOMAIN : "react-redux-firebase-6ae9c.firebaseapp.com",
-  databaseURL: process.env.NODE_ENV === 'production' ? DATABASE_URL : "https://react-redux-firebase-6ae9c.firebaseio.com",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
 }
