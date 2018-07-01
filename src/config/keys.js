a8d0acdc5ce251d6c4464699076f0642bc6a8689
Mikees23 <michael23.jd@gmail.com>
2018-07-01 17:28:14 +0600

    Change keys

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,9 @@
+export const API_KEY = process.env.API_KEY;
+export const AUTH_DOMAIN = process.env.AUTH_DOMAIN;
+export const DATABASE_URL = process.env.DATABASE_URL;
+
 export const FirebaseConfig = {
-  apiKey: process.env.NODE_ENV === 'production' ? process.env.API_KEY : "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
-  authDomain: process.env.NODE_ENV === 'production' ? process.env.AUTH_DOMAIN : "react-redux-firebase-6ae9c.firebaseapp.com",
-  databaseURL: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : "https://react-redux-firebase-6ae9c.firebaseio.com",
+  apiKey: process.env.NODE_ENV === 'production' ? API_KEY : "AIzaSyC6ASXQqPFqIa-rHWajpsJkf7sA0VZAOrQ",
+  authDomain: process.env.NODE_ENV === 'production' ? AUTH_DOMAIN : "react-redux-firebase-6ae9c.firebaseapp.com",
+  databaseURL: process.env.NODE_ENV === 'production' ? DATABASE_URL : "https://react-redux-firebase-6ae9c.firebaseio.com",
 }
